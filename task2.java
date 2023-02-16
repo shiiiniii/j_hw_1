184f4abfb63918783e5d00b6dc5d10f04aa5d7fe
shiiiniii <115028625+shiiiniii@users.noreply.github.com>
2023-02-16 20:44:12 +0300

    Create task2.java

---
+++ task2.java
@@ -0,0 +1,13 @@
+// 2. Вывести все простые числа от 1 до 1000 - готово
+
+package tasks_1;
+import java.util.Scanner;
+public class Program{
+	public static void main(String[] args) {
+		try (Scanner tru = new Scanner(System.in)) {
+			for(int i = 1; i < 10001; i++){
+				     System.out.println(i);
+				}
+			}
+		}
+}
